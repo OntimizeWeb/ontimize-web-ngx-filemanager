1647dee430affd501dca305270b3fd561ad5e977
gonzalo.martinez <gonzalo.martinez@imatia.com>
2018-04-30 09:17:18 +0200

    Updating code

---
+++ src/core.ts
@@ -0,0 +1,3 @@
+export * from './core/file.class';
+export * from './core/o-filemanager-translate.pipe';
+export * from './core/filename.validator';
--- src/core/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './file.class';
-export * from './o-filemanager-translate.pipe';
-export * from './filename.validator';
