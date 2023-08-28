0502c6bdd14616d93cee75f3e0369bef6d16294c
Patricia Martinez <32088627+patricia-martinez-imatia@users.noreply.github.com>
2023-08-28 09:06:56 +0200

    [Migration] Changes from 8.5.6 to 15.0.0 (#120)
    * Merge pull request #108 from OntimizeWeb/release/8.6.0
    
    Release 8.6.0
    
    * Merge pull request #111 from OntimizeWeb/hotfix/8.6.1
    
    Hotfix 8.6.1
    
    ---------
    
    Co-authored-by: Daniel Graña Cousido <daniel.grana@imatia.com>

--- projects/ontimize-web-ngx-filemanager/src/lib/components/filemanager-table/o-filemanager-table.component.ts
+++ projects/ontimize-web-ngx-filemanager/src/lib/components/filemanager-table/o-filemanager-table.component.ts
@@ -2,25 +2,31 @@ import { HttpEventType } from '@angular/common/http';
 import { AfterViewInit, Component, forwardRef, Inject, Injector, OnDestroy, OnInit, Optional, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { DialogService, InputConverter, OFormComponent, OnClickTableEvent, OTranslateService } from 'ontimize-web-ngx';
-import { Subscription } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
+import { WorkspaceS3 } from '../../interfaces/workspaceS3.interface';
 import { DomService } from '../../services/dom.service';
 import { FileManagerStateService } from '../../services/filemanager-state.service';
+import { WorkspaceService } from '../../services/workspace.service';
 import { FileClass } from '../../util/file.class';
 import { OFileManagerTranslatePipe } from '../../util/o-filemanager-translate.pipe';
 import { OFileInputExtendedComponent } from '../file-input/o-file-input-extended.component';
 import { DownloadProgressComponent } from '../status/download/download-progress.component';
 import { UploadProgressComponent } from '../status/upload/upload-progress.component';
 import { ChangeNameDialogComponent, ChangeNameDialogData } from './table-extended/dialog/changename/change-name-dialog.component';
+import { CopyDialogComponent, CopyDialogData } from './table-extended/dialog/copy/copy-dialog.component';
 import { OTableExtendedComponent } from './table-extended/o-table-extended.component';
 
 export const DEFAULT_INPUTS_O_FILEMANAGER_TABLE = [
   'workspaceKey: workspace-key',
+  /*Funtion called for creating the workspace S3.The function return a object of type WorkspaceS3*/
+  'workspaceS3: workspace-s3',
   'service',
   'parentKeys: parent-keys',
   'autoHideUpload : auto-hide-upload',
   'autoHideTimeout : auto-hide-timeout',
   'serviceType : service-type',
+  'type',
   'newFolderButton: new-folder-button',
   'selectAllCheckbox: select-all-checkbox',
   'enabled'
@@ -43,12 +49,19 @@ export const DEFAULT_OUTPUTS_O_FILEMANAGER_TABLE = [
   providers: [{
     provide: FileManagerStateService,
     useClass: FileManagerStateService
-  }]
+  }, WorkspaceService]
 })
 export class OFileManagerTableComponent implements OnInit, OnDestroy, AfterViewInit {
+  public isS3Type: BehaviorSubject<boolean> = new BehaviorSubject(true);
 
-  public static DEFAULT_SERVICE_TYPE = 'FileManagerService';
+  public static DEFAULT_SERVICE_TYPE = 'FileManagerOntimizeService';
+  public static DEFAULT_TYPE = 'Ontimize';
 
+  public static S3_SERVICE_TYPE = 'FileManagerS3Service';
+  public static S3_TYPE = 'S3';
+
+  type: string = OFileManagerTableComponent.DEFAULT_SERVICE_TYPE;
+  workspaceS3: (values: { [key: string]: any }) => WorkspaceS3;
   workspaceKey: string;
   service: string;
   parentKeys: string;
@@ -70,6 +83,8 @@ export class OFileManagerTableComponent implements OnInit, OnDestroy, AfterViewI
 
   protected uploadMethod: string = 'upload';
   protected downloadMethod: string = 'download';
+  protected copyMethod: string = 'copyFiles';
+  protected moveMethod: string = 'moveFiles';
 
   protected onFormDataSubscribe: Subscription;
   protected stateService: FileManagerStateService;
@@ -91,6 +106,8 @@ export class OFileManagerTableComponent implements OnInit, OnDestroy, AfterViewI
   protected uploadProgressComponentRef: any;
   protected fileChangeSubscription: Subscription;
 
+  protected workspaceService: WorkspaceService;
+
   protected downloadProgressComponentRef: any;
 
   constructor(
@@ -103,6 +120,7 @@ export class OFileManagerTableComponent implements OnInit, OnDestroy, AfterViewI
     this.dialogService = this.injector.get(DialogService);
     this.domService = this.injector.get(DomService);
     this.dialog = this.injector.get(MatDialog);
+    this.workspaceService = this.injector.get(WorkspaceService);
 
     const self = this;
     if (this.oForm) {
@@ -119,8 +137,14 @@ export class OFileManagerTableComponent implements OnInit, OnDestroy, AfterViewI
   }
 
   ngOnInit() {
-    if (!this.serviceType) {
+    this.isS3Type.next(this.type === OFileManagerTableComponent.S3_TYPE);
+    if (!this.serviceType && this.type === OFileManagerTableComponent.DEFAULT_SERVICE_TYPE) {
       this.serviceType = OFileManagerTableComponent.DEFAULT_SERVICE_TYPE;
+      this.workspaceService.initializeOntimizeProvider(this.workspaceKey, this.oForm);
+    }
+    else if (!this.serviceType && this.type === OFileManagerTableComponent.S3_TYPE) {
+      this.serviceType = OFileManagerTableComponent.S3_SERVICE_TYPE;
+      this.workspaceService.initializeS3Provider(this.workspaceKey, this.oForm, this.workspaceS3);
     }
     this.oTable.setStateService(this.stateService);
   }
@@ -180,6 +204,14 @@ export class OFileManagerTableComponent implements OnInit, OnDestroy, AfterViewI
     return this.translatePipe.transform('CONTEXT_MENU.DOWNLOAD_FILE');
   }
 
+  get copyLabel(): string {
+    return this.translatePipe.transform('CONTEXT_MENU.COPY');
+  }
+
+  get moveLabel(): string {
+    return this.translatePipe.transform('CONTEXT_MENU.MOVE');
+  }
+
   get changeNameLabel(): string {
     return this.translatePipe.transform('CONTEXT_MENU.CHANGE_NAME');
   }
@@ -235,7 +267,7 @@ export class OFileManagerTableComponent implements OnInit, OnDestroy, AfterViewI
   onContextDownloadFile() {
     const tableService = this.oTable.getDataService();
     if (tableService && (this.downloadMethod in tableService) && (this.oTable.getSelectedItems().length > 0)) {
-      const workspaceId = (this.oForm as any).getDataValue(this.workspaceKey).value;
+      const workspaceId = this.workspaceService.getWorkspace();
       const selectedItems = this.oTable.getSelectedItems();
       let downloadId = undefined;
       if (selectedItems.length > 1 || (selectedItems.length === 1 && selectedItems[0].directory)) {
@@ -260,8 +292,82 @@ export class OFileManagerTableComponent implements OnInit, OnDestroy, AfterViewI
         } else {
           this.dialogService.alert('ERROR', this.translatePipe.transform('MESSAGES.ERROR_DOWNLOAD'));
         }
+        self.updateDownloaderStatus(downloadId, { cancelled: true });
       });
     }
+  }
+
+  private onContextCopyAndMoveHelper(event: any, keyDialogTitle: string, action: any): void {
+    if (event && event.data) {
+      let folder: string = event.data.rowValue.directoryPath;
+      if (this.oTable.getSelectedItems().length > 0) folder = this.oTable.getSelectedItems()[0].directoryPath;
+
+      let dialogData: CopyDialogData = {
+        title: keyDialogTitle,
+        placeholder: 'targetFolder',
+        defaultValue: folder,
+        fileData: event.data.rowValue
+      };
+
+      let cfg: MatDialogConfig = {
+        role: 'dialog',
+        disableClose: false,
+        data: dialogData,
+        panelClass: ['o-dialog-class']
+      };
+      let dialogRef = this.dialog.open(CopyDialogComponent, cfg);
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          action(result).bind(this);
+        }
+      });
+    }
+  }
+
+  private copyAndMoveHelper(folder: string, method: any, loading: BehaviorSubject<boolean>, keyMsgError: string): void {
+    const tableService = this.oTable.getDataService();
+    if (tableService && (this.downloadMethod in tableService) && (this.oTable.getSelectedItems().length > 0)) {
+      loading.next(true);
+      const workspaceId = this.workspaceService.getWorkspace();
+      const selectedItems = this.oTable.getSelectedItems();
+      const kv = {};
+      const currentFilter = this.stateService.getCurrentQueryFilter();
+      if (currentFilter.hasOwnProperty(OTableExtendedComponent.FM_FOLDER_PARENT_KEY)) {
+        kv[OTableExtendedComponent.FM_FOLDER_PARENT_KEY] = currentFilter[OTableExtendedComponent.FM_FOLDER_PARENT_KEY];
+      }
+      const self = this;
+      tableService[method](workspaceId, selectedItems, folder, kv)
+        .subscribe(result => {
+          self.oTable.reloadCurrentFolder();
+        }, err => {
+          loading.next(false);
+          if (err && typeof err !== 'object') {
+            this.dialogService.alert('ERROR', err);
+          } else {
+            this.dialogService.alert('ERROR', this.translatePipe.transform(keyMsgError));
+          }
+        }, () => {
+          loading.next(false);
+        });
+    }
+  }
+
+  onContextCopy(event: any): void {
+    this.onContextCopyAndMoveHelper(event, 'COPY_TITLE', this.copy.bind(this));
+  }
+
+  copy(folder: string): void {
+    const loadingCopySubject: BehaviorSubject<boolean> = this.oTable.loadingCopySubject;
+    this.copyAndMoveHelper(folder, this.copyMethod, loadingCopySubject, 'MESSAGES.ERROR_COPY');
+  }
+
+  onContextMove(event): void {
+    this.onContextCopyAndMoveHelper(event, 'MOVE_TITLE', this.move.bind(this));
+  }
+
+  move(folder: string): void {
+    const loadingMoveSubject: BehaviorSubject<boolean> = this.oTable.loadingMoveSubject;
+    this.copyAndMoveHelper(folder, this.moveMethod, loadingMoveSubject, 'MESSAGES.ERROR_MOVE');
   }
 
   onContextChangeName(event): void {
@@ -292,14 +398,20 @@ export class OFileManagerTableComponent implements OnInit, OnDestroy, AfterViewI
   changeFileName(name: string, file: FileClass): void {
     let tableService = this.oTable.getDataService();
     if (tableService && (this.changeNameMethod in tableService)) {
+      const loadingRenameSubject: BehaviorSubject<boolean> = this.oTable.loadingRenameSubject;
+      loadingRenameSubject.next(true);
       let self = this;
-      tableService[this.changeNameMethod](name, file).subscribe(() => {
-        // do nothing
+      const workspaceId = this.workspaceService.getWorkspace();
+      tableService[this.changeNameMethod](name, file, workspaceId).subscribe(() => {
       }, err => {
+        loadingRenameSubject.next(false);
         if (err && typeof err !== 'object') {
-          self.dialogService.alert('ERROR', err);
+          this.dialogService.alert('ERROR', err);
+        } else {
+          this.dialogService.alert('ERROR', this.translatePipe.transform('MESSAGES.ERROR_CHANGE_FILE'));
         }
       }, () => {
+        loadingRenameSubject.next(false);
         self.oTable.reloadCurrentFolder();
       });
     }
@@ -335,8 +447,7 @@ export class OFileManagerTableComponent implements OnInit, OnDestroy, AfterViewI
     }
     if (val && this.uploadProgressComponentRef) {
       const instance: UploadProgressComponent = this.uploadProgressComponentRef.instance;
-      let files = this.oFileInput.uploader.files.filter(item => !item.isUploaded && !item.isCancel);
-
+      let files = this.oFileInput.uploader.files.filter(item => !item.isUploaded && !item.isCancel)
 
       instance.uploaderFiles = createComp ? files : instance.uploaderFiles.concat(files);
 
